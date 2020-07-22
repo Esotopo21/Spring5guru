c0a9fdd30032016abfef556e85dddc1c727bdf15
burlac <cristiano.burla@prometeia.com>
2020-07-22 11:28:31 +0200

    first commit

---
+++ src/main/java/it/burla/springgurucourse/SpringgurucourseApplication.java
@@ -0,0 +1,13 @@
+package it.burla.springgurucourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgurucourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringgurucourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/burla/springgurucourse/SpringgurucourseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.burla.springgurucourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgurucourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
